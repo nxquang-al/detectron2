3fbb784974410b698e4d51e4576057c9245c9f4d
Nguyễn Xuân Quang <79528257+nxquang2002@users.noreply.github.com>
2022-10-02 17:17:26 +0700

    Update fpn.py

--- detectron2/modeling/backbone/fpn.py
+++ detectron2/modeling/backbone/fpn.py
@@ -196,8 +196,10 @@ class LastLevelP6P7(nn.Module):
     def __init__(self, in_channels, out_channels, in_feature="res5"):
         super().__init__()
         self.num_levels = 2
-        self.in_feature = in_feature
-        self.p6 = nn.Conv2d(in_channels, out_channels, 3, 2, 1)
+        # self.in_feature = in_feature
+        self.in_feature = "p5"
+        # self.p6 = nn.Conv2d(in_channels, out_channels, 3, 2, 1)
+        self.p6 = nn.Conv2d(out_channels, out_channels, 3, 2, 1)
         self.p7 = nn.Conv2d(out_channels, out_channels, 3, 2, 1)
         for module in [self.p6, self.p7]:
             weight_init.c2_xavier_fill(module)
